02ac96a052d277b2c865bca1aa39ff7f114282a6
Benjamin Clark <bclark@sudoblark.com>
2024-10-08 14:50:29 +0100

    Bump example version

--- examples/email_and_lambda/main.tf
+++ examples/email_and_lambda/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 module "sns" {
-  source = "github.com/sudoblark/sudoblark.terraform.module.aws.sns?ref=1.0.3"
+  source = "github.com/sudoblark/sudoblark.terraform.module.aws.sns?ref=1.1.0"
 
   application_name = var.application_name
   environment      = var.environment
